50d1010f729b54e9638fa38bef1bfd6372fa5aad
Geziel Santos <geziel.santos@NB-TI-GEZIEL.democrata.com.br>
2024-12-27 10:59:51 -0300

    adicionando rotas do CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,43 @@
 from http import HTTPStatus
-from fastapi.testclient import TestClient
-from fast_zero.app import app
 
-client = TestClient(app)
+# def teste_read_root_deve_retornar_ok_e_ola_mundo():
+#     client = TestClient(app)
 
-def teste_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+#     response = client.get('/')
 
-    response = client.get('/')
+#     assert response.status_code == HTTPStatus.OK
+#     assert response.json() == {'Ola Mundo'}
+
+
+def teste_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'gesiel',
+            'password': '123456',
+            'email': 'gesiel@hotmail.com.br',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+
+    assert response.json() == {
+        'username': 'gesiel',
+        'email': 'gesiel@hotmail.com.br',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'Ola Mundo Marte'}
-
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'gesiel',
+                'email': 'gesiel@hotmail.com.br',
+                'id': 1,
+            }
+        ]
+    }
